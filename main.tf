9292efc6a262964b0d1c792f5485d9e3282440ce
IB <53451213+IBMESP@users.noreply.github.com>
2025-09-26 10:32:53 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name = var.storage_account_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name = var.container_name
+  storage_account_name = azurerm_storage_account.storage.name
+  //storage_account_id = azurerm_storage_account.storage.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "blob" {
+  name = "blob.zip"
+  storage_account_name = azurerm_storage_account.storage.name
+  storage_container_name = azurerm_storage_container.container.name
+  type = "Block"
+  source = "teapot.zip"
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  type = string
+  default = "Italy North"
+}
+
+variable "resource_group_name" {
+  type = string
+  default = "rg-blob-storage"  
+}
+
+variable "storage_account_name" {
+  type = string
+  default = "blobdemo1941123"
+}
+
+variable "container_name" {
+  type = string
+  default = "container-demo"
+}
